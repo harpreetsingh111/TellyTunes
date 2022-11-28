15972157cfe335916404b0324b84403a032357b4
amanjot singh <amanjotsingh@asite.com>
2022-11-28 13:29:36 +0530

    test mail and design settings

---
+++ mail.php
@@ -0,0 +1,35 @@
+<?php
+$to = "harpreets@evolvan.com";
+$subject = "Test-mail";
+
+$message = "
+<html>
+<head>
+<title>test-mail</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: harpreets@evolvan.com' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>
